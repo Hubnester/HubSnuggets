1e29428ae3956b60cf6d6421849c6e8b4fbb9412
Hubnester <jamesdolphin001@gmail.xom>
2021-02-14 14:04:17 +1000

    Cosmetics

--- scripts/hubsnuggetplayer.lua
+++ scripts/hubsnuggetplayer.lua
@@ -6,6 +6,18 @@ function init()
 	
 	self.hubsnuggetConfig = root.assetJson("/species/hubsnugget.species")
 	self.hubsnuggetRace = player.species()
+	
+	if self.hubsnuggetRace == "hubsnugget" and not status.statusProperty("hubsnuggetJustCreated") then
+		local slots = {"chest", "legs"}
+		for _, slot in ipairs (slots) do
+			local item = player.equippedItem(slot) or {}
+			if self.hubsnuggetConfig.allowedCosmetics[item.name] then
+				player.setEquippedItem(slot .. "Cosmetic", item)
+				player.setEquippedItem(slot, nil)
+			end
+		end
+		status.setStatusProperty("hubsnuggetJustCreated", true)
+	end
 end
 
 function update(dt)
@@ -13,7 +25,7 @@ function update(dt)
 	
 	-- Improve this eventually
 	if self.hubsnuggetRace == "hubsnugget" then
-		local slots = { "headCosmetic", "chestCosmetic", "legsCosmetic", "backCosmetic"}
+		local slots = {"headCosmetic", "chestCosmetic", "legsCosmetic", "backCosmetic" }
 		for _, slot in ipairs (slots) do
 			local item = player.equippedItem(slot) or {}
 			if not self.hubsnuggetConfig.allowedCosmetics[item.name] then
